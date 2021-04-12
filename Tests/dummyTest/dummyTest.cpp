cc33501f0a8ec32237a40b0ad2bdcd9a4d2bf385
tobre1 <t.reiter1@live.de>
2021-04-12 19:16:35 +0200

    add folder structure and CMakeLists

---
+++ Tests/dummyTest/dummyTest.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <rtDummy.hpp>
+
+int main()
+{
+    std::cout << "Dummy test" << std::endl;
+    return 0;
+}
---
+++ include/rtDummy.hpp
@@ -0,0 +1,12 @@
+#ifndef RT_DUMMY_HPP
+#define RT_DUMMY_HPP
+
+#include <embree3/rtcore.h>
+#include <lsDomain.hpp>
+
+class rtDummy
+{
+    
+};
+
+#endif // RT_DUMMY_HPP
