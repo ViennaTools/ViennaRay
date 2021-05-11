37ce62d52c59bcad8e4bea0cf28fa34653d20115
tobre1 <t.reiter1@live.de>
2021-05-11 18:02:41 +0200

    Ray Tracing Example

--- Examples/Trench/Trench.cpp
+++ Examples/Trench/Trench.cpp
@@ -1,12 +1,58 @@
 #include <rtTrace.hpp>
 #include <rtBoundCondition.hpp>
 #include <rtTraceDirection.hpp>
-#include <lsDomain.hpp>
-#include <lsMakeGeometry.hpp>
-#include <lsBooleanOperation.hpp>
-#include <lsToDiskMesh.hpp>
-#include <lsVTKWriter.hpp>
+#include <rtReflectionDiffuse.hpp>
 #include <omp.h>
+
+template <typename NumericType, int D = 3>
+void writeVTK(std::string filename,
+              const std::vector<rtTriple<NumericType>> &points,
+              const std::vector<NumericType> &mcestimates,
+              const std::vector<NumericType> &mcEstNorm)
+{
+    std::ofstream f(filename.c_str());
+
+    f << "# vtk DataFile Version 2.0" << std::endl;
+    f << D << "D Surface" << std::endl;
+    f << "ASCII" << std::endl;
+    f << "DATASET UNSTRUCTURED_GRID" << std::endl;
+    f << "POINTS " << points.size() << " float" << std::endl;
+
+    for (unsigned int i = 0; i < points.size(); i++)
+    {
+        for (int j = 0; j < 3; j++)
+            f << static_cast<float>(points[i][j]) << " ";
+        f << std::endl;
+    }
+
+    f << "CELLS " << points.size() << " " << points.size() * 2 << std::endl;
+    size_t c = 0;
+    for (unsigned int i = 0; i < points.size(); i++)
+    {
+        f << 1 << " " << c++ << std::endl;
+    }
+
+    f << "CELL_TYPES " << points.size() << std::endl;
+    for (unsigned i = 0; i < points.size(); ++i)
+        f << 1 << std::endl;
+
+    f << "CELL_DATA " << mcestimates.size() << std::endl;
+    f << "SCALARS mc-estimates float" << std::endl;
+    f << "LOOKUP_TABLE default" << std::endl;
+    for (unsigned j = 0; j < mcestimates.size(); ++j)
+    {
+        f << ((std::abs(mcestimates[j]) < 1e-6) ? 0.0 : mcestimates[j]) << std::endl;
+    }
+
+    f << "SCALARS mc-est-no-smooth float" << std::endl;
+    f << "LOOKUP_TABLE default" << std::endl;
+    for (unsigned j = 0; j < mcEstNorm.size(); ++j)
+    {
+        f << ((std::abs(mcEstNorm[j]) < 1e-6) ? 0.0 : mcEstNorm[j]) << std::endl;
+    }
+
+    f.close();
+}
 
 int main()
 {
@@ -14,70 +60,31 @@ int main()
 
     using NumericType = float;
     using ParticleType = rtParticle2<NumericType>;
-    using ReflectionType = rtReflectionSpecular<NumericType, D>;
+    using ReflectionType = rtReflectionDiffuse<NumericType, D>;
 
     omp_set_num_threads(12);
 
-    NumericType extent = 30;
-    NumericType gridDelta = 0.5;
-    double bounds[2 * D] = {-extent, extent, -extent, extent, -extent, extent};
-    lsDomain<NumericType, D>::BoundaryType boundaryCons[D];
-    boundaryCons[0] = lsDomain<NumericType, D>::BoundaryType::REFLECTIVE_BOUNDARY;
-    boundaryCons[1] = lsDomain<NumericType, D>::BoundaryType::REFLECTIVE_BOUNDARY;
-    boundaryCons[2] = lsDomain<NumericType, D>::BoundaryType::INFINITE_BOUNDARY;
-
-    auto dom = lsSmartPointer<lsDomain<NumericType, D>>::New(bounds, boundaryCons,
-                                                             gridDelta);
-
-    {
-        NumericType origin[3] = {0., 0., 0.};
-        NumericType planeNormal[3] = {0., 0., 1.};
-        auto plane =
-            lsSmartPointer<lsPlane<NumericType, D>>::New(origin, planeNormal);
-        lsMakeGeometry<NumericType, D>(dom, plane).apply();
-    }
-
-    // // Create trench geometry
-    // {
-    //     auto trench = lsSmartPointer<lsDomain<NumericType, D>>::New(
-    //         bounds, boundaryCons, gridDelta);
-    //     NumericType minCorner[D] = {-extent - 1, -extent / 4.f, -30.};
-    //     NumericType maxCorner[D] = {extent + 1, extent / 4.f, 1.};
-    //     auto box = lsSmartPointer<lsBox<NumericType, D>>::New(minCorner, maxCorner);
-    //     lsMakeGeometry<NumericType, D>(trench, box).apply();
-    //     lsBooleanOperation<NumericType, D>(
-    //         dom, trench, lsBooleanOperationEnum::RELATIVE_COMPLEMENT)
-    //         .apply();
-    // }
+    NumericType gridDelta;
+    std::vector<rtTriple<NumericType>> points;
+    std::vector<rtTriple<NumericType>> normals;
+    rtInternal::readGridFromFile("trenchGrid3D.dat", gridDelta, points, normals);
 
     rtTraceBoundary bC[D];
     bC[0] = rtTraceBoundary::PERIODIC;
     bC[1] = rtTraceBoundary::PERIODIC;
     bC[2] = rtTraceBoundary::PERIODIC;
 
-
     rtTrace<NumericType, ParticleType, ReflectionType, D> rayTracer;
+    rayTracer.setGeometry(points, normals, gridDelta);
     rayTracer.setSourceDirection(rtTraceDirection::POS_Z);
     rayTracer.setNumberOfRaysPerPoint(1000);
     rayTracer.setCosinePower(5.);
     rayTracer.setBoundaryConditions(bC);
-
-    auto mesh = lsSmartPointer<lsMesh<NumericType>>::New();
-    lsToDiskMesh<NumericType, D>(dom, mesh).apply();
-    auto points = mesh->getNodes();
-    auto normals = *mesh->getVectorData("Normals");
-
-    rayTracer.setGeometry(points, normals, gridDelta);
-
-    std::cout << "Ray tracing ... " << std::endl;
     rayTracer.apply();
-    auto mcestimates = rayTracer.getMcEstimates();
-    auto error = rayTracer.getRelativeError();
 
-    mesh->insertNextScalarData(mcestimates, "mc-estimates");
-    mesh->insertNextScalarData(error, "rel-error");
-
-    lsVTKWriter<NumericType>(mesh, "trench_grid_plane_periodicBC.vtk").apply();
+    auto mcEstimates = rayTracer.getMcEstimates();
+    auto mcEstNorm = rayTracer.getMcEstimatesNorm();
+    writeVTK("trenchResult.vtk", points, mcEstimates, mcEstNorm);
 
     return 0;
 }
