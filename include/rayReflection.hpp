7157f225e02ee1df92df17e615a21254a1d9b4ac
tobre1 <t.reiter1@live.de>
2021-07-04 11:13:42 +0200

    Removed reflection objects

--- include/rayReflection.hpp
+++ include/rayReflection.hpp
@@ -25,4 +25,49 @@ rayReflectionSpecular(const rayTriple<NumericType> &rayDir,
   return direction;
 }
 
+template <typename NumericType, int D>
+rayTriple<NumericType>
+rayReflectionDiffuse(const rayTriple<NumericType> &geomNormal, rayRNG &RNG) {
+  assert(rayInternal::IsNormalized(geomNormal) &&
+         "rayReflectionDiffuse: Surface normal is not normalized");
+
+  if constexpr (D == 3) {
+    // Compute lambertian reflection with respect to surface normal
+    const auto basis = rayInternal::getOrthonormalBasis(geomNormal);
+
+    std::uniform_real_distribution<NumericType> uniDist;
+    auto r1 = uniDist(RNG);
+    auto r2 = uniDist(RNG);
+
+    constexpr NumericType two_pi = 2 * rayInternal::PI;
+    NumericType cc1 = sqrt(r2);
+    NumericType cc2 = cos(two_pi * r1) * sqrt(1 - r2);
+    NumericType cc3 = sin(two_pi * r1) * sqrt(1 - r2);
+
+    auto tt1 = basis[0];
+    rayInternal::Scale(cc1, tt1);
+    auto tt2 = basis[1];
+    rayInternal::Scale(cc2, tt2);
+    auto tt3 = basis[2];
+    rayInternal::Scale(cc3, tt3);
+    auto newDirection = rayInternal::Sum(tt1, tt2, tt3);
+    assert(rayInternal::IsNormalized(newDirection) &&
+           "rayReflectionDiffuse: New direction is not normalized");
+
+    return newDirection;
+  } else {
+    const auto angle =
+        ((NumericType)RNG() / (NumericType)RNG.max() - 0.5) * rayInternal::PI;
+    const auto cos = std::cos(angle);
+    const auto sin = std::sin(angle);
+    auto newDirection =
+        rayTriple<NumericType>{cos * geomNormal[0] - sin * geomNormal[1],
+                               sin * geomNormal[0] + cos * geomNormal[1], 0.};
+    assert(rayInternal::IsNormalized(newDirection) &&
+           "rayReflectionDiffuse: New direction is not normalized");
+
+    return newDirection;
+  }
+}
+
 #endif // RAY_REFLECTION_HPP
