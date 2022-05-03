29160d7520fafd19cbfe512c4313c119603783ef
bobinac <bobinac@iue.tuwien.ac.at>
2022-05-03 16:25:57 +0200

    Added coned cosine reflection

--- include/rayReflection.hpp
+++ include/rayReflection.hpp
@@ -25,6 +25,68 @@ rayReflectionSpecular(const rayTriple<NumericType> &rayDir,
   return direction;
 }
 
+template <typename NumericType>
+static rayTriple<NumericType>
+rayReflectionConedCosine(NumericType coneAngle,
+                         const rayTriple<NumericType> &specDirection,
+                         rayRNG &RNG) {
+
+  std::uniform_real_distribution<NumericType> uniDist;
+  NumericType u, sqrt_1m_u;
+  NumericType angle;
+  do {
+    u = std::sqrt(uniDist(RNG));
+    sqrt_1m_u = std::sqrt(1. - u);
+    angle = coneAngle * sqrt_1m_u; // random angle between 0 and specular angle
+  } while (uniDist(RNG) * angle * u >
+           std::cos(rayInternal::PI/2. * sqrt_1m_u) * std::sin(angle));
+
+  NumericType costheta = std::cos(angle);
+
+  NumericType cosphi, sinphi;
+  NumericType r2;
+
+  do {
+    cosphi = uniDist(RNG) - 0.5;
+    sinphi = uniDist(RNG) - 0.5;
+    r2 = cosphi * cosphi + sinphi * sinphi;
+  } while (r2 >= 0.25 || r2 <= 1e-10);
+
+  //Rotate(AverageDirection, RandomDirection, sinphi, cosphi, costheta, r2);
+  
+  rayTriple<NumericType> randomDir;
+  costheta = std::min(costheta, 1.);
+
+  NumericType a0;
+  NumericType a1;
+
+  if (std::fabs(specDirection[0]) <= std::fabs(specDirection[1])) {
+    a0 = specDirection[0];
+    a1 = specDirection[1];
+  } else {
+    a0 = specDirection[1];
+    a1 = specDirection[0];
+  }
+
+  const NumericType a0_a0_m1 = 1. - a0 * a0;
+  const NumericType tmp =
+      std::sqrt(std::max(1. - costheta * costheta, 0.) / (r2 * a0_a0_m1));
+  const NumericType tmp_sinphi = tmp * sinphi;
+  const NumericType tmp_cosphi = tmp * cosphi;
+  const NumericType costheta_p_a0_tmp_sinphi = costheta + a0 * tmp_sinphi;
+
+  randomDir[0] = a0 * costheta - a0_a0_m1 * tmp_sinphi;
+  randomDir[1] =
+      a1 * costheta_p_a0_tmp_sinphi + specDirection[2] * tmp_cosphi;
+  randomDir[2] =
+      specDirection[2] * costheta_p_a0_tmp_sinphi - a1 * tmp_cosphi;
+
+  if (a0 != specDirection[0])
+    std::swap(randomDir[0], randomDir[1]);
+
+  return randomDir;
+}
+
 template <typename NumericType, int D>
 static rayTriple<NumericType>
 rayReflectionDiffuse(const rayTriple<NumericType> &geomNormal, rayRNG &RNG) {
