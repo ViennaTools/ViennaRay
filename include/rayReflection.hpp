aaad59e8ebbe14c58692f82e66e03b4abf9f443d
bobinac <bobinac@iue.tuwien.ac.at>
2022-05-10 15:19:32 +0200

    Adjusted the rayReflectionConedCosine() to prevent reflecting rays into the geometry

--- include/rayReflection.hpp
+++ include/rayReflection.hpp
@@ -6,8 +6,7 @@
 #include <rayUtil.hpp>
 
 template <typename NumericType>
-static rayTriple<NumericType>
-PickRandomPointOnUnitSphere(rayRNG &RNG) { 
+static rayTriple<NumericType> PickRandomPointOnUnitSphere(rayRNG &RNG) {
   std::uniform_real_distribution<NumericType> uniDist;
   NumericType x, y, z, x2, y2, x2py2;
   do {
@@ -71,24 +70,128 @@ rayReflectionSpecular(const rayTriple<NumericType> &rayDir,
 }
 
 template <typename NumericType>
-static rayTriple<NumericType>
-rayReflectionConedCosine(NumericType coneAngle,
-                         const rayTriple<NumericType> &specDirection,  // TODO: rays with incident angle higher than minAngle can be reflected into the geometry as the average direction is not modified accordingly
-                         rayRNG &RNG) {                                // rotate the avg reflected direction to match the minimum angle
-  
+static rayTriple<NumericType> rayReflectionConedCosine(
+    NumericType coneAngle, const rayTriple<NumericType> &rayDir,
+    const rayTriple<NumericType> &geomNormal, rayRNG &RNG) {
+
+  assert(rayInternal::IsNormalized(geomNormal) &&
+         "rayReflectionSpecular: Surface normal is not normalized");
+  assert(rayInternal::IsNormalized(rayDir) &&
+         "rayReflectionSpecular: Surface normal is not normalized");
+
+  // Calculate specular direction
+  auto dirOldInv = rayInternal::Inv(rayDir);
+
+  auto specDirection = rayInternal::Diff(
+      rayInternal::Scale(2 * rayInternal::DotProduct(geomNormal, dirOldInv),
+                         geomNormal),
+      dirOldInv);
+
   std::uniform_real_distribution<NumericType> uniDist;
   NumericType u, sqrt_1m_u;
   NumericType angle;
+  rayTriple<NumericType> randomDir;
+
+  //  loop until ray is reflected away from the surface normal
+  //  this loop takes care of the case where part of the cone points
+  //  into the geometry
+  do {
+    do { // generate a random angle between 0 and specular angle
+      u = std::sqrt(uniDist(RNG));
+      sqrt_1m_u = std::sqrt(1. - u);
+      angle = coneAngle * sqrt_1m_u;
+    } while (uniDist(RNG) * angle * u >
+             std::cos(rayInternal::PI / 2. * sqrt_1m_u) * std::sin(angle));
+
+    // Random Azimuthal Rotation
+    NumericType costheta = std::max(std::min(std::cos(angle), 1.), 0.);
+    NumericType cosphi, sinphi;
+    NumericType r2;
+
+    do {
+      cosphi = uniDist(RNG) - 0.5;
+      sinphi = uniDist(RNG) - 0.5;
+      r2 = cosphi * cosphi + sinphi * sinphi;
+    } while (r2 >= 0.25 || r2 <= std::numeric_limits<NumericType>::epsilon());
+
+    // Rotate
+    costheta = std::min(costheta, 1.);
+
+    NumericType a0;
+    NumericType a1;
+
+    if (std::fabs(specDirection[0]) <= std::fabs(specDirection[1])) {
+      a0 = specDirection[0];
+      a1 = specDirection[1];
+    } else {
+      a0 = specDirection[1];
+      a1 = specDirection[0];
+    }
+
+    const NumericType a0_a0_m1 = 1. - a0 * a0;
+    const NumericType tmp =
+        std::sqrt(std::max(1. - costheta * costheta, 0.) / (r2 * a0_a0_m1));
+    const NumericType tmp_sinphi = tmp * sinphi;
+    const NumericType tmp_cosphi = tmp * cosphi;
+    const NumericType costheta_p_a0_tmp_sinphi = costheta + a0 * tmp_sinphi;
+
+    randomDir[0] = a0 * costheta - a0_a0_m1 * tmp_sinphi;
+    randomDir[1] =
+        a1 * costheta_p_a0_tmp_sinphi + specDirection[2] * tmp_cosphi;
+    randomDir[2] =
+        specDirection[2] * costheta_p_a0_tmp_sinphi - a1 * tmp_cosphi;
+
+    if (a0 != specDirection[0])
+      std::swap(randomDir[0], randomDir[1]);
+  } while (rayInternal::DotProduct(randomDir, geomNormal) <= 0.);
+
+  return randomDir;
+}
+
+template <typename NumericType>
+static rayTriple<NumericType>
+rayReflectionConedCosine2(const rayTriple<NumericType> &rayDir,
+                          const rayTriple<NumericType> &geomNormal, rayRNG &RNG,
+                          NumericType &minAvgConeAngle = 0.) {
+
+  assert(rayInternal::IsNormalized(geomNormal) &&
+         "rayReflectionSpecular: Surface normal is not normalized");
+  assert(rayInternal::IsNormalized(rayDir) &&
+         "rayReflectionSpecular: Surface normal is not normalized");
+
+  auto dirOldInv = rayInternal::Inv(rayDir);
+
+  // Compute average direction
+  auto specDirection = rayInternal::Diff(
+      rayInternal::Scale(2 * rayInternal::DotProduct(geomNormal, dirOldInv),
+                         geomNormal),
+      dirOldInv);
+
+  // Compute incidence angle
+  auto cosTheta = -rayInternal::DotProduct(rayDir, geomNormal);
+
+  assert(cosTheta >= 0 && "Hit backside of disc");
+  assert(cosTheta <= 1 + 1e-6 && "Error in calculating cos theta");
+
+  const NumericType incAngle = std::acos(std::max(std::min(cosTheta, 1.), 0.));
+
+  NumericType coneAngle =
+      std::max(rayInternal::PI / 2. - incAngle, minAvgConeAngle);
+
+  std::uniform_real_distribution<NumericType> uniDist;
+  NumericType u, sqrt_1m_u;
   // generate a random angle between 0 and specular angle
+  NumericType angle;
   do {
     u = std::sqrt(uniDist(RNG));
     sqrt_1m_u = std::sqrt(1. - u);
-    angle = coneAngle * sqrt_1m_u; 
+    angle = coneAngle * sqrt_1m_u;
   } while (uniDist(RNG) * angle * u >
            std::cos(rayInternal::PI / 2. * sqrt_1m_u) * std::sin(angle));
 
+  NumericType costheta = std::cos(angle);
+
   // Random Azimuthal Rotation
-  NumericType costheta = std::max(std::min(std::cos(angle), 1.), 0.);
   NumericType cosphi, sinphi;
   NumericType r2;
 
@@ -96,7 +199,7 @@ rayReflectionConedCosine(NumericType coneAngle,
     cosphi = uniDist(RNG) - 0.5;
     sinphi = uniDist(RNG) - 0.5;
     r2 = cosphi * cosphi + sinphi * sinphi;
-  } while (r2 >= 0.25 || r2 <= 1e-10);
+  } while (r2 >= 0.25 || r2 <= std::numeric_limits<NumericType>::epsilon());
 
   rayTriple<NumericType> randomDir;
 
@@ -112,92 +215,6 @@ rayReflectionConedCosine(NumericType coneAngle,
   } else {
     a0 = specDirection[1];
     a1 = specDirection[0];
-  }
-
-  const NumericType a0_a0_m1 = 1. - a0 * a0;
-  const NumericType tmp =
-      std::sqrt(std::max(1. - costheta * costheta, 0.) / (r2 * a0_a0_m1));
-  const NumericType tmp_sinphi = tmp * sinphi;
-  const NumericType tmp_cosphi = tmp * cosphi;
-  const NumericType costheta_p_a0_tmp_sinphi = costheta + a0 * tmp_sinphi;
-
-  randomDir[0] = a0 * costheta - a0_a0_m1 * tmp_sinphi;
-  randomDir[1] = a1 * costheta_p_a0_tmp_sinphi + specDirection[2] * tmp_cosphi;
-  randomDir[2] = specDirection[2] * costheta_p_a0_tmp_sinphi - a1 * tmp_cosphi;
-
-  if (a0 != specDirection[0])
-    std::swap(randomDir[0], randomDir[1]);
-
-  return randomDir;
-}
-
-template <typename NumericType>
-static rayTriple<NumericType>
-rayReflectionConedCosine2(const rayTriple<NumericType> &rayDir,
-                         const rayTriple<NumericType> &geomNormal,
-                         rayRNG &RNG,
-                         NumericType& minAvgConeAngle = 0.) {    
-
-  assert(rayInternal::IsNormalized(geomNormal) &&
-         "rayReflectionSpecular: Surface normal is not normalized");
-  assert(rayInternal::IsNormalized(rayDir) &&
-         "rayReflectionSpecular: Surface normal is not normalized");   
-
-  auto dirOldInv = rayInternal::Inv(rayDir);
-
-  // Compute average direction
-  auto avgDirection = rayInternal::Diff(
-      rayInternal::Scale(2 * rayInternal::DotProduct(geomNormal, dirOldInv),
-                         geomNormal),
-      dirOldInv);   
-
-  // Compute incidence angle
-  auto cosTheta = -rayInternal::DotProduct(rayDir, geomNormal);
-
-  assert(cosTheta >= 0 && "Hit backside of disc");
-  assert(cosTheta <= 1 + 1e-6 && "Error in calculating cos theta");
-
-  const NumericType incAngle = std::acos(std::max(std::min(cosTheta, 1.),0.));
-
-  NumericType coneAngle = std::max(rayInternal::PI/2. - incAngle, minAvgConeAngle);
-  
-  std::uniform_real_distribution<NumericType> uniDist;
-  NumericType u, sqrt_1m_u;
-  // generate a random angle between 0 and specular angle
-  NumericType angle;
-  do {
-    u = std::sqrt(uniDist(RNG));
-    sqrt_1m_u = std::sqrt(1. - u);
-    angle = coneAngle * sqrt_1m_u; 
-  } while (uniDist(RNG) * angle * u >
-           std::cos(rayInternal::PI / 2. * sqrt_1m_u) * std::sin(angle));
-
-  NumericType costheta = std::cos(angle);
-  
-  // Random Azimuthal Rotation
-  NumericType cosphi, sinphi;
-  NumericType r2;
-
-  do {
-    cosphi = uniDist(RNG) - 0.5;
-    sinphi = uniDist(RNG) - 0.5;
-    r2 = cosphi * cosphi + sinphi * sinphi;
-  } while (r2 >= 0.25 || r2 <= 1e-10);
-
-  rayTriple<NumericType> randomDir;
-
-  // Rotate
-  costheta = std::min(costheta, 1.);
-
-  NumericType a0;
-  NumericType a1;
-
-  if (std::fabs(avgDirection[0]) <= std::fabs(avgDirection[1])) {
-    a0 = avgDirection[0];
-    a1 = avgDirection[1];
-  } else {
-    a0 = avgDirection[1];
-    a1 = avgDirection[0];
   }
 
   const NumericType a0_a0_m1 = 1. - a0 * a0;
@@ -277,9 +294,9 @@ rayReflectionDiffuse2(const rayTriple<NumericType> &geomNormal, rayRNG &RNG) {
   else
     randomDirection[2] = 0;
 
-   rayInternal::Normalize(randomDirection);
-   assert(rayInternal::IsNormalized(randomDirection) &&
-          "rayReflectionDiffuse: New direction is not normalized");
+  rayInternal::Normalize(randomDirection);
+  assert(rayInternal::IsNormalized(randomDirection) &&
+         "rayReflectionDiffuse: New direction is not normalized");
   return randomDirection;
 }
 
