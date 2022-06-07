459815da6d35b7a7cf8d7e4418cb788eb74a9b92
Tobias Reiter <t.reiter1@live.de>
2022-06-07 21:36:45 +0200

    Code format

--- include/rayUtil.hpp
+++ include/rayUtil.hpp
@@ -37,8 +37,9 @@ struct rayTraceInfo {
 namespace rayInternal {
 constexpr double PI = 3.14159265358979323846;
 
-template<int D>
-constexpr double DiskFactor = 0.5 * (D == 3 ? 1.7320508 : 1.41421356237) * (1 + 1e-5);
+template <int D>
+constexpr double DiskFactor = 0.5 * (D == 3 ? 1.7320508 : 1.41421356237) *
+                              (1 + 1e-5);
 
 /* ------------- Vector operation functions ------------- */
 template <typename NumericType>
