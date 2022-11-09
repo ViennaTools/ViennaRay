6ed969aab7d60860af7765f85183b6b8f4676200
Tobias Reiter <44025882+tobre1@users.noreply.github.com>
2022-11-09 11:07:28 +0100

    Added relative error checking (#31)
    * Added relative error checking
    
    * Added error checking test
    
    * Changed index type for OpenMP loops in Windows

---
+++ Tests/errorChecking/errorChecking.cpp
@@ -0,0 +1,35 @@
+#include <rayParticle.hpp>
+#include <rayTestAsserts.hpp>
+#include <rayTrace.hpp>
+
+int main() {
+  constexpr int D = 3;
+  using NumericType = float;
+  omp_set_num_threads(4);
+
+  NumericType extent = 5;
+  NumericType gridDelta = 0.5;
+  std::vector<std::array<NumericType, D>> points;
+  std::vector<std::array<NumericType, D>> normals;
+  rayInternal::createPlaneGrid(gridDelta, extent, {0, 1, 2}, points, normals);
+
+  rayTraceBoundary boundaryConds[D];
+  boundaryConds[0] = rayTraceBoundary::REFLECTIVE;
+  boundaryConds[1] = rayTraceBoundary::REFLECTIVE;
+  boundaryConds[2] = rayTraceBoundary::REFLECTIVE;
+  auto particle = std::make_unique<rayTestParticle<NumericType>>();
+
+  rayTrace<NumericType, D> rayTracer;
+  rayTracer.setParticleType(particle);
+  rayTracer.setGeometry(points, normals, gridDelta);
+  rayTracer.setBoundaryConditions(boundaryConds);
+  rayTracer.setSourceDirection(rayTraceDirection::POS_Z);
+  rayTracer.setNumberOfRaysPerPoint(10);
+  rayTracer.setUseRandomSeeds(false);
+  rayTracer.apply();
+
+  auto info = rayTracer.getRayTraceInfo();
+  RAYTEST_ASSERT(info.warning);
+
+  return 0;
+}
