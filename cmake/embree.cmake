92e948b0e83fc1c6622299b598c7723b0ec8c635
Noah <37805707+Curve@users.noreply.github.com>
2024-03-11 11:09:37 +0100

    refactor: `setup_embree_env` behavior (#45)

--- cmake/embree.cmake
+++ cmake/embree.cmake
@@ -1,22 +1,15 @@
 macro(setup_embree_env TARGET OUTPUT)
-  if(VIENNARAY_DISABLE_COPY
-     OR NOT MSVC
-     OR VIENNARAY_SYSTEM_EMBREE)
-    message(STATUS "[ViennaRay] Skipping Embree-Environment setup for ${TARGET}")
-  else()
     message(STATUS "[ViennaRay] Setting up Embree-Environment for ${TARGET}")
-    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY $<1:${PROJECT_BINARY_DIR}/${OUTPUT}>)
 
     add_custom_command(
       TARGET ${TARGET}
       POST_BUILD
       COMMAND ${CMAKE_COMMAND} -E copy_directory $<TARGET_FILE_DIR:tbb>
-              ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+              ${OUTPUT})
 
     add_custom_command(
       TARGET ${TARGET}
       POST_BUILD
       COMMAND ${CMAKE_COMMAND} -E copy_directory $<TARGET_FILE_DIR:embree>
-              ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
-  endif()
+              ${OUTPUT})
 endmacro()
