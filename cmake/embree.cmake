49b9c281a96069387313a1e83bd91e12674df43a
Curve <fynnbwdt@gmail.com>
2024-06-09 20:16:42 +0200

    feat: add viennacore

--- cmake/embree.cmake
+++ cmake/embree.cmake
@@ -1,6 +1,11 @@
 macro(setup_embree_env TARGET OUTPUT)
   message(STATUS "[ViennaRay] Setting up Embree-Environment for ${TARGET}")
 
+  if(NOT TARGET embree)
+    message(WARNING "[ViennaRay] Could not find Embree-Target")
+    return()
+  endif()
+
   add_custom_command(
     TARGET ${TARGET}
     POST_BUILD
