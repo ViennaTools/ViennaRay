4e8dacaf9ee94b6406d2a36725438e7b665efef7
Tobias Reiter <t.reiter1@live.de>
2025-02-03 12:04:45 +0100

    Update coned cosine reflection

--- tests/reflection/reflection.cpp
+++ tests/reflection/reflection.cpp
@@ -1,4 +1,5 @@
 #include <rayReflection.hpp>
+#include <vcTimer.hpp>
 
 #include <fstream>
 
@@ -6,50 +7,83 @@ using namespace viennaray;
 
 int main() {
 
-  RNG rngState;
+  using NumericType = double;
+  constexpr int N = 50000;
+  RNG rngState(12351263);
+  Timer timer;
 
   {
+    std::array<Vec3D<NumericType>, N> directions;
+
     // diffuse reflection
-    Vec3D<double> normal = {0.0, 0.0, 1.0};
+    Vec3D<NumericType> normal = {0.0, 0.0, 1.0};
+
+    timer.start();
+    for (int i = 0; i < N; ++i) {
+      directions[i] = ReflectionDiffuse<NumericType, 3>(normal, rngState);
+    }
+    timer.finish();
+
+    std::cout << "Time for " << N
+              << " diffuse reflections: " << timer.currentDuration * 1e-6
+              << " ms" << std::endl;
+
     std::ofstream file("diffuse_reflection.txt");
-    for (int i = 0; i < 1000; ++i) {
-      Vec3D<double> dir = ReflectionDiffuse<double, 3>(normal, rngState);
+    for (auto const &dir : directions) {
       file << dir[0] << " " << dir[1] << " " << dir[2] << std::endl;
     }
     file.close();
   }
 
-  Vec3D<double> normal = {0.0, 0.0, 1.0};
-  double const minAvgConeAngle = M_PI_2 / 5;
-  double const minAngle = 1.3962634;
-  Vec3D<double> rayDir = {0.0, -1.0, -1.0};
-  Normalize(rayDir);
+  Vec3D<NumericType> normal = {0.0, 0.0, 1.0};
+  NumericType const minAngle = 85.0 * M_PI / 180.0;
+  NumericType incAngle = 75.0 * M_PI / 180.0;
+  Vec3D<NumericType> rayDir = {0.0, -std::sin(incAngle), -std::cos(incAngle)};
+  auto coneAngle = M_PI_2 - std::min(incAngle, minAngle);
 
-  auto cosTheta = -DotProduct(rayDir, normal);
-  std::cout << "cosTheta: " << cosTheta << std::endl;
-  auto incAngle = std::acos(cosTheta);
+  std::cout << "minAngle: " << minAngle << std::endl;
   std::cout << "incAngle: " << incAngle << std::endl;
   std::cout << "incAngle [deg]: " << incAngle * 180.0 / M_PI << std::endl;
-  auto coneAngle = M_PI_2 - std::min(incAngle, minAngle);
-  //   auto coneAngle = std::max(incAngle, minAngle);
+  std::cout << "coneAngle: " << coneAngle << std::endl;
 
   {
+    std::array<Vec3D<NumericType>, N> directions;
+
     // coned specular reflection
+    timer.start();
+    for (int i = 0; i < N; ++i) {
+      directions[i] = ReflectionConedCosine<NumericType, 3>(
+          rayDir, normal, rngState, coneAngle);
+    }
+    timer.finish();
+
+    std::cout << "Time for " << N
+              << " coned specular reflections: " << timer.currentDuration * 1e-6
+              << " ms" << std::endl;
+
     std::ofstream file("coned_specular_reflection.txt");
-    for (int i = 0; i < 1000; ++i) {
-      Vec3D<double> dir =
-          ReflectionConedCosine<double, 3>(rayDir, normal, rngState, coneAngle);
+    for (auto const &dir : directions) {
       file << dir[0] << " " << dir[1] << " " << dir[2] << std::endl;
     }
     file.close();
   }
 
   {
-    // coned specular reflection old
+    std::array<Vec3D<NumericType>, N> directions;
+
+    // coned specular reflection
+    timer.start();
+    for (int i = 0; i < N; ++i) {
+      directions[i] = rayInternal::ReflectionConedCosineOld<NumericType, 3>(
+          rayDir, normal, rngState, coneAngle);
+    }
+    timer.finish();
+
+    std::cout << "Time for " << N << " coned specular reflections (old): "
+              << timer.currentDuration * 1e-6 << " ms" << std::endl;
+
     std::ofstream file("coned_specular_reflection_old.txt");
-    for (int i = 0; i < 1000; ++i) {
-      Vec3D<double> dir = rayInternal::ReflectionConedCosineOld<double, 3>(
-          coneAngle, rayDir, normal, rngState);
+    for (auto const &dir : directions) {
       file << dir[0] << " " << dir[1] << " " << dir[2] << std::endl;
     }
     file.close();
